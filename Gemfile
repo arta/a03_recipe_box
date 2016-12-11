d32b7954b6e4cc93f670158c2779b7ab2358e12d
Martin Fencl <martin.fencl@gmail.com>
2016-12-11 13:36:33 -0600

    Add image upload to recipes using paperclip gem.

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # 12-11-16:
+gem 'paperclip', '~> 5.1'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 # 12-9-16:
 gem 'simple_form', '~> 3.3', '>= 3.3.1'
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ApplicationRecord
   # belongs_to :user
 
+  has_attached_file :image, styles: { medium: '400x400#', thumb:' 100x100#' }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   # validates :title, presence:true
 end
