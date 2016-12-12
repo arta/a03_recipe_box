b0ec328f56c3509677136d337f9e8370edd3b062
Martin Fencl <martin.fencl@gmail.com>
2016-12-12 16:13:44 -0600

    Generate models Ingredient and Direction. Migrate. Nest their attributes in Recipe _form using cocoon (allows their dynamic adding and removing on Recipe /new and /edit view).

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,16 @@
 class Recipe < ApplicationRecord
   # belongs_to :user
 
+  # https://www.sitepoint.com/better-nested-attributes-in-rails-with-the-cocoon-gem/
+  has_many :ingredients, dependent: :destroy, inverse_of: :recipe
+  has_many :directions,  dependent: :destroy, inverse_of: :recipe
+
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy:true
+  accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy:true
+
   has_attached_file :image, styles: { medium: '400x400#', thumb:' 100x100#' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  # validates :title, presence:true
+  validates :title, :description, presence:true
+  validates :image, presence:true, on: :create # if image.empty?
 end
